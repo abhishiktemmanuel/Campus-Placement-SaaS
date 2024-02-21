8f2f6083ea2cfaef9463a8a6a3df02e6fe0e13d9
Abhishikt <deepikaabhiprakash02@gmail.com>
2024-02-21 13:54:04 +0530

    connected mongodb

---
+++ server/src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+import { DB_NAME } from "../constant.js";
+
+const connectDB = async () => {
+  dotenv.config();
+
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connceted !! DB HOST: ${connectionInstance.connection.host} \n`
+    );
+  } catch (error) {
+    console.log("Error while connecting to MongoDB", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,13 @@
+import dotenv from "dotenv"
 import mongoose  from 'mongoose';
-import { DB_NAME } from './constant';
+import { DB_NAME } from './constant.js';
+import connectDB from './db/index.js';
 
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
 
 
 
