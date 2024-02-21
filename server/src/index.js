166a8fbe63d807c5b3e4b9a33613d48075afc68d
Abhishikt <deepikaabhiprakash02@gmail.com>
2024-02-21 10:49:41 +0530

    database is in a different continent

---
+++ server/src/index.js
@@ -0,0 +1,37 @@
+import mongoose  from 'mongoose';
+import { DB_NAME } from './constant';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+const app = express()
+
+;(async()=>{
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/$
+        {DB_NAME}`)
+        app.on("error", (error)=>{
+            console.log('Error while connecting to MongoDB', error)
+            throw error
+        })
+
+        app.listen(process.env.PORT, () =>{
+            console.log(`Server is running on port ${process.env.PORT}`)
+        })
+    } catch(error){
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()*/
