073308bf349b8635533e6d151217473603cd74a6
Abhishikt <deepikaabhiprakash02@gmail.com>
2024-03-04 11:53:06 +0530

    util code

--- server/src/app.js
+++ server/src/app.js
@@ -14,5 +14,6 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
