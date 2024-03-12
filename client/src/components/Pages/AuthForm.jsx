8b29945b3b63d7622597d09823a97d457cc24135
Abhishikt <deepikaabhiprakash02@gmail.com>
2024-03-12 13:57:36 +0530

    Update translation transform values in AuthForm component

--- client/src/components/Pages/AuthForm.jsx
+++ client/src/components/Pages/AuthForm.jsx
@@ -24,10 +24,10 @@ const AuthForm = () => {
 
       <div className={` mx-6 max-w-md overflow-hidden transition-all duration-500 ease-in-out bg-white bg-opacity-20 rounded-3xl backdrop-blur-lg shadow-2xl  ${isRegistering ? 'py-16' : 'py-8'}`}>
         <div className="flex">
-          <div className={` w-full justify-center items-center transition-transform duration-500 ease-in-out ${isRegistering ? '-translate-x-[400px]' : 'translate-x-1/2'}`}>
+          <div className={` w-full justify-center items-center transition-transform duration-500 ease-in-out ${isRegistering ? '-translate-x-[500px]' : 'translate-x-1/2'}`}>
             <LoginForm toggleForm={toggleForm} />
           </div>
-          <div className={` w-full justify-center items-center transition-transform duration-500 ease-in-out ${isRegistering ? '-translate-x-1/2' : 'translate-x-[400px]'}`}>
+          <div className={` w-full justify-center items-center transition-transform duration-500 ease-in-out ${isRegistering ? '-translate-x-1/2' : 'translate-x-[500px]'}`}>
             <SignUpForm toggleForm={toggleForm} />
           </div>
         </div>
